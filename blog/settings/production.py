e2e3929bd801bc37665926f4d0c2bdc793c7ba3c
Martin Rafael Cruz <44838498+MaRafa2011@users.noreply.github.com>
2020-01-11 21:17:49 -0600

    Add files via upload

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -3,15 +3,19 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rafadjangoblog.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'dde6h9c42kcgtu',
+        'USER':'otvvqjucpjcgfy',
+        'PASSWORD':'68e454bfb71c050f8a4e17bcbf7a4ee1f82d6b772b3179cb87b974e7c73479e1',
+        'HOST':'ec2-174-129-32-200.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
 
